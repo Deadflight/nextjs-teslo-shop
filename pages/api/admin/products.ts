50ae24977a695b262febb537c25424e179c426cc
Carlos <correamillancarlos@gmail.com>
2022-07-15 16:12:55 -0400

    fin admin section

---
+++ pages/api/admin/products.ts
@@ -0,0 +1,147 @@
+import { isValidObjectId } from "mongoose";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { IProduct } from "../../../interfaces/products";
+import { Product } from "../../../models";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config(process.env.CLOUDINARY_URL || "");
+
+type Data = { message: string } | IProduct | IProduct[];
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	switch (req.method) {
+		case "GET":
+			return getProducts(req, res);
+
+		case "PUT":
+			return updateProduct(req, res);
+		case "POST":
+			return createProduct(req, res);
+		default:
+			return res.status(400).json({ message: "Bad request" });
+	}
+}
+
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	await db.connect();
+
+	const products = await Product.find().sort({ title: "asc" }).lean();
+
+	await db.disconnect();
+
+	// Un Procesamiento de las imagenes cuando las subamos al server
+
+	const updatedProducts = products.map((product) => {
+		product.images = product.images.map((image) => {
+			return image.includes("http")
+				? image
+				: `${process.env.HOST_NAME}/products/${image}`;
+		});
+
+		return product;
+	});
+
+	res.status(200).json(updatedProducts);
+};
+
+const updateProduct = async (
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) => {
+	const { _id = "", images = [] } = req.body as IProduct;
+
+	if (!isValidObjectId(_id)) {
+		return res.status(400).json({ message: "El id del producto no es válido" });
+	}
+
+	if (images.length < 2) {
+		return res
+			.status(400)
+			.json({ message: "El producto debe tener al menos 2 imágenes" });
+	}
+
+	//TODO: posiblemente tendremos un localhost:3000/products/asdasd.jpg
+
+	try {
+		await db.connect();
+
+		const product = await Product.findById(_id);
+
+		if (!product) {
+			await db.disconnect();
+			return res
+				.status(404)
+				.json({ message: "No existe un producto con ese id" });
+		}
+
+		//TODO: delete images in cloudinary
+		//https://res.cloudinary.com/carlos-dev/image/upload/v1657904500/z2cbpgh7bavyuscqvi2p.webp
+
+		product.images.forEach(async (image) => {
+			if (!images.includes(image)) {
+				// Delete image in cloudinary
+
+				const [fileId, extension] = image
+					.substring(image.lastIndexOf("/") + 1)
+					.split(".");
+
+				console.log({ image, extension, fileId });
+
+				await cloudinary.uploader.destroy(fileId);
+			}
+		});
+
+		await product.update(req.body);
+
+		await db.disconnect();
+
+		return res.status(200).json(product);
+	} catch (error) {
+		console.log(error);
+		await db.disconnect();
+		return res.status(500).json({ message: "Error al actualizar el producto" });
+	}
+};
+
+const createProduct = async (
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) => {
+	const { images = [] } = req.body as IProduct;
+
+	if (images.length < 2) {
+		return res
+			.status(400)
+			.json({ message: "El producto debe tener al menos 2 imágenes" });
+	}
+
+	//TODO: posiblemente tendremos un localhost:3000/products/asdasd.jpg
+
+	try {
+		await db.connect();
+
+		const productInDB = await Product.findOne({ slug: req.body.slug });
+
+		if (productInDB) {
+			await db.disconnect();
+			return res
+				.status(400)
+				.json({ message: "El producto ya existe con ese slug" });
+		}
+
+		const product = new Product(req.body);
+
+		await product.save();
+		await db.disconnect();
+
+		return res.status(201).json(product);
+	} catch (error) {
+		console.log(error);
+		await db.disconnect();
+		return res.status(500).json({ message: "Revisar los logs del servidor" });
+	}
+};
