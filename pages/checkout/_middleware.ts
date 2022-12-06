d34d29243a79cff2efc8e03ed815bebc971308e7
Carlos <correamillancarlos@gmail.com>
2022-12-06 10:53:16 -0400

    middleware secret

--- pages/checkout/_middleware.ts
+++ pages/checkout/_middleware.ts
@@ -1,35 +1,32 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 // import { jwt } from '../../utils';
 
+export async function middleware(req: NextRequest | any, ev: NextFetchEvent) {
+	const session = await getToken({
+		req,
+		secret: process.env.NEXTAUTH_SECRET! || "",
+	});
 
-export async function middleware( req: NextRequest | any, ev: NextFetchEvent ) {
+	// console.log({ session });
 
-    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+	if (!session) {
+		const requestedPage = req.page.name;
+		return NextResponse.redirect(`/auth/login?p=${requestedPage}`);
+	}
 
-    // console.log({ session });
+	return NextResponse.next();
 
-    if ( !session ) {
-        const requestedPage = req.page.name;
-        return NextResponse.redirect(`/auth/login?p=${ requestedPage }`);
-    }
-
-    return NextResponse.next();
-
-
-    // const { token = '' } = req.cookies;
-    // // return new Response('No autorizado', {
-    // //     status: 401
-    // // });
-    // try {
-    //     await jwt.isValidToken( token );
-    //     return NextResponse.next();
-    // } catch (error) {
-    //     // return Response.redirect('/auth/login');
-    //     const requestedPage = req.page.name;
-    //     return NextResponse.redirect(`/auth/login?p=${ requestedPage }`);
-    // }
-
+	// const { token = '' } = req.cookies;
+	// // return new Response('No autorizado', {
+	// //     status: 401
+	// // });
+	// try {
+	//     await jwt.isValidToken( token );
+	//     return NextResponse.next();
+	// } catch (error) {
+	//     // return Response.redirect('/auth/login');
+	//     const requestedPage = req.page.name;
+	//     return NextResponse.redirect(`/auth/login?p=${ requestedPage }`);
+	// }
 }
-
-
