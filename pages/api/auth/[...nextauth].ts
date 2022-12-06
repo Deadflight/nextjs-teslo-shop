327a076b937f5fa46e7adc205955a451349ab8a3
Carlos <correamillancarlos@gmail.com>
2022-12-06 10:16:04 -0400

    nextauth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,92 +1,102 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import Credentials from 'next-auth/providers/credentials';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
-import { dbUsers } from '../../../database';
+import { dbUsers } from "../../../database";
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    
-    // ...add more providers here
+	// Configure one or more authentication providers
+	providers: [
+		// ...add more providers here
 
-    Credentials({
-      name: 'Custom Login',
-      credentials: {
-        email: { label: 'Correo:', type: 'email', placeholder: 'correo@google.com'  },
-        password: { label: 'Contraseña:', type: 'password', placeholder: 'Contraseña'  },
-      },
-      async authorize(credentials) {
-        console.log({credentials})
-        // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
+		Credentials({
+			name: "Custom Login",
+			credentials: {
+				email: {
+					label: "Correo:",
+					type: "email",
+					placeholder: "correo@google.com",
+				},
+				password: {
+					label: "Contraseña:",
+					type: "password",
+					placeholder: "Contraseña",
+				},
+			},
+			async authorize(credentials) {
+				console.log({ credentials });
+				// return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
 
-        return await dbUsers.checkUserEmailPassword( credentials!.email, credentials!.password );
+				return await dbUsers.checkUserEmailPassword(
+					credentials!.email,
+					credentials!.password
+				);
+			},
+		}),
 
-      }
-    }),
+		GithubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+	],
 
+	// Custom Pages
+	pages: {
+		signIn: "/auth/login",
+		newUser: "/auth/register",
+	},
+	secret: process.env.NEXT_PUBLIC_SECRET,
 
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+	// // Callbacks
+	// jwt: {
+	// 	// secret: process.env.JWT_SECRET_SEED, // deprecated
+	// },
 
+	session: {
+		maxAge: 2592000, /// 30d
+		strategy: "jwt",
+		updateAge: 86400, // cada día
+	},
 
-  ],
+	callbacks: {
+		async jwt({ token, account, user }) {
+			// console.log({ token, account, user });
 
-  // Custom Pages
-  pages: {
-    signIn: '/auth/login',
-    newUser: '/auth/register'
-  },
+			if (account) {
+				token.accessToken = account.access_token;
 
-  // Callbacks
-  jwt: {
-    // secret: process.env.JWT_SECRET_SEED, // deprecated
-  },
-  
-  session: {
-    maxAge: 2592000, /// 30d
-    strategy: 'jwt',
-    updateAge: 86400, // cada día
-  },
+				switch (account.type) {
+					case "oauth":
+						token.user = await dbUsers.oAUthToDbUser(
+							user?.email || "",
+							user?.name || ""
+						);
+						break;
 
+					case "credentials":
+						token.user = user;
+						break;
+				}
+			}
 
-  callbacks: {
+			return token;
+		},
 
-    async jwt({ token, account, user }) {
-      // console.log({ token, account, user });
+		async session({
+			session,
+			token,
+			user,
+		}: {
+			session: any;
+			token: any;
+			user: any;
+		}) {
+			// console.log({ session, token, user });
 
-      if ( account ) {
-        token.accessToken = account.access_token;
+			session.accessToken = token.accessToken;
+			session.user = token.user as any;
 
-        switch( account.type ) {
-
-          case 'oauth': 
-            token.user = await dbUsers.oAUthToDbUser( user?.email || '', user?.name || '' );
-          break;
-
-          case 'credentials':
-            token.user = user;
-          break;
-        }
-
-      }
-
-      return token;
-    },
-
-
-    async session({ session, token, user }){
-      // console.log({ session, token, user });
-
-      session.accessToken = token.accessToken;
-      session.user = token.user as any;
-
-      return session;
-    }
-    
-
-  }
-
+			return session;
+		},
+	},
 });
