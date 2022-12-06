7e9f8f882902c5699a5795b0a0cabd35f4b5d558
Carlos <correamillancarlos@gmail.com>
2022-12-06 11:22:22 -0400

    session provider

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req: NextRequest | any, ev: NextFetchEvent) {
 	const session: any = await getToken({
 		req,
-		secret: "wdPesjEG3rsRZIKJt20iS5TmpL89hpVcV3oVXuizbVU=",
+		secret: process.env.NEXTAUTH_SECRET! || "",
 	});
 
 	if (!session) {
