89d2931bb475392785b1bb3df43b459867cdf6ea
Carlos <correamillancarlos@gmail.com>
2023-01-23 21:47:02 -0400

    api folder change

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,111 +1,94 @@
-import { useState, useEffect } from 'react';
-import { PeopleOutline } from '@mui/icons-material'
-import useSWR from 'swr';
+import { useState, useEffect } from "react";
+import { PeopleOutline } from "@mui/icons-material";
+import useSWR from "swr";
 
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Grid, Select, MenuItem } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { Grid, Select, MenuItem } from "@mui/material";
 
-import { AdminLayout } from '../../components/layouts'
-import { IUser } from '../../interfaces';
-import { tesloApi } from '../../api';
-
-
-
+import { AdminLayout } from "../../components/layouts";
+import { IUser } from "../../interfaces";
+import { tesloApi } from "../../axios";
 
 const UsersPage = () => {
+	const { data, error } = useSWR<IUser[]>("/api/admin/users");
+	const [users, setUsers] = useState<IUser[]>([]);
 
-    const { data, error } = useSWR<IUser[]>('/api/admin/users');
-    const [ users, setUsers ] = useState<IUser[]>([]);
+	useEffect(() => {
+		if (data) {
+			setUsers(data);
+		}
+	}, [data]);
 
+	if (!data && !error) return <></>;
 
-    useEffect(() => {
-      if (data) {
-          setUsers(data);
-      }
-    }, [data])
-    
+	const onRoleUpdated = async (userId: string, newRole: string) => {
+		const previosUsers = users.map((user) => ({ ...user }));
+		const updatedUsers = users.map((user) => ({
+			...user,
+			role: userId === user._id ? newRole : user.role,
+		}));
 
-    if ( !data && !error ) return (<></>);
+		setUsers(updatedUsers);
 
-    const onRoleUpdated = async( userId: string, newRole: string ) => {
+		try {
+			await tesloApi.put("/admin/users", { userId, role: newRole });
+		} catch (error) {
+			setUsers(previosUsers);
+			console.log(error);
+			alert("No se pudo actualizar el role del usuario");
+		}
+	};
 
-        const previosUsers = users.map( user => ({ ...user }));
-        const updatedUsers = users.map( user => ({
-            ...user,
-            role: userId === user._id ? newRole : user.role
-        }));
+	const columns: GridColDef[] = [
+		{ field: "email", headerName: "Correo", width: 250 },
+		{ field: "name", headerName: "Nombre completo", width: 300 },
+		{
+			field: "role",
+			headerName: "Rol",
+			width: 300,
+			renderCell: ({ row }: GridValueGetterParams) => {
+				return (
+					<Select
+						value={row.role}
+						label="Rol"
+						onChange={({ target }) => onRoleUpdated(row.id, target.value)}
+						sx={{ width: "300px" }}
+					>
+						<MenuItem value="admin"> Admin </MenuItem>
+						<MenuItem value="client"> Client </MenuItem>
+						<MenuItem value="super-user"> Super User </MenuItem>
+						<MenuItem value="SEO"> SEO </MenuItem>
+					</Select>
+				);
+			},
+		},
+	];
 
-        setUsers(updatedUsers);
+	const rows = users.map((user) => ({
+		id: user._id,
+		email: user.email,
+		name: user.name,
+		role: user.role,
+	}));
 
-        try {
-            
-            await tesloApi.put('/admin/users', {  userId, role: newRole });
+	return (
+		<AdminLayout
+			title={"Usuarios"}
+			subTitle={"Mantenimiento de usuarios"}
+			icon={<PeopleOutline />}
+		>
+			<Grid container className="fadeIn">
+				<Grid item xs={12} sx={{ height: 650, width: "100%" }}>
+					<DataGrid
+						rows={rows}
+						columns={columns}
+						pageSize={10}
+						rowsPerPageOptions={[10]}
+					/>
+				</Grid>
+			</Grid>
+		</AdminLayout>
+	);
+};
 
-        } catch (error) {
-            setUsers( previosUsers );
-            console.log(error);
-            alert('No se pudo actualizar el role del usuario');
-        }
-
-    }
-
-
-    const columns: GridColDef[] = [
-        { field: 'email', headerName: 'Correo', width: 250 },
-        { field: 'name', headerName: 'Nombre completo', width: 300 },
-        {
-            field: 'role', 
-            headerName: 'Rol', 
-            width: 300,
-            renderCell: ({row}: GridValueGetterParams) => {
-                return (
-                    <Select
-                        value={ row.role }
-                        label="Rol"
-                        onChange={ ({ target }) => onRoleUpdated( row.id, target.value ) }
-                        sx={{ width: '300px' }}
-                    >
-                        <MenuItem value='admin'> Admin </MenuItem>
-                        <MenuItem value='client'> Client </MenuItem>
-                        <MenuItem value='super-user'> Super User </MenuItem>
-                        <MenuItem value='SEO'> SEO </MenuItem>
-                    </Select>
-                )
-            }
-        },
-    ];
-
-    const rows = users.map( user => ({
-        id: user._id,
-        email: user.email,
-        name: user.name,
-        role: user.role
-    }))
-
-
-  return (
-    <AdminLayout 
-        title={'Usuarios'} 
-        subTitle={'Mantenimiento de usuarios'}
-        icon={ <PeopleOutline /> }
-    >
-
-
-        <Grid container className='fadeIn'>
-            <Grid item xs={12} sx={{ height:650, width: '100%' }}>
-                <DataGrid 
-                    rows={ rows }
-                    columns={ columns }
-                    pageSize={ 10 }
-                    rowsPerPageOptions={ [10] }
-                />
-
-            </Grid>
-        </Grid>
-
-
-    </AdminLayout>
-  )
-}
-
-export default UsersPage
+export default UsersPage;
